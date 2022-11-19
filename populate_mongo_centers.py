8db4d1ff3f6da943794687a0687e72b0743155d6
Andrei Gogonea <andrei.gogonea@keysight.com>
2022-11-19 17:42:24 +0200

    Add db center populate script

---
+++ populate_mongo_centers.py
@@ -0,0 +1,28 @@
+# Python program to read
+# json file
+  
+  
+import json
+  
+# Opening JSON file
+
+with open('test.json', 'r') as f:
+  
+    # returns JSON object as 
+    # a dictionary
+    data = f.read()
+    data = eval(data)
+
+
+import pymongo  # package for working with MongoDB
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+pomodoro_db = mongo_client['pomodoro']
+centers_collection = pomodoro_db['centers']
+
+for center in data:
+    print(center)
+    centers_collection.insert_one(center)
+
+    
+  
+
