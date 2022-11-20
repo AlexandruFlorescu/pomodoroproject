1a213be4f502e6e516c22eae48575f6a94754e6a
Andrei Gogonea <andrei.gogonea@keysight.com>
2022-11-20 03:48:17 +0200

    Update population script

--- populate_mongo_centers.py
+++ populate_mongo_centers.py
@@ -20,6 +20,7 @@ pomodoro_db = mongo_client['pomodoro']
 centers_collection = pomodoro_db['centers']
 
 for center in data:
+    center['coordinates'] = {"lat": center['coordinates'][0], "lng": center['coordinates'][1]}
     print(center)
     centers_collection.insert_one(center)
 
